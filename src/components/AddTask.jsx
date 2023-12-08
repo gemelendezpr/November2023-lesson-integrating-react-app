f1dab75330e5b1777c38dda71ed0caf4815c81bb
Dustin Hurtt <dustin_hurtt@outlook.com>
2023-12-08 12:26:25 -0500

    lesson done

---
+++ src/components/AddTask.jsx
@@ -0,0 +1,67 @@
+// src/components/AddTask.jsx
+
+import { useState } from "react";
+import axios from "axios";
+
+const API_URL = "https://project-management-api-4641927fee65.herokuapp.com";
+
+
+function AddTask({ refreshProject, projectId }) {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+
+  projectId = Number(projectId)
+
+  
+  const handleSubmit = (e) => {
+    e.preventDefault();
+ 
+    // We need the project id when creating the new task
+    // Create an object representing the body of the POST request
+    const requestBody = { title, description, projectId };
+ 
+    axios
+      .post(`${API_URL}/tasks`, requestBody)
+      .then((response) => {
+
+        console.log("Adding task ===>", response.data)
+        // Reset the state to clear the inputs
+        setTitle("");
+        setDescription("");
+      
+        // Invoke the callback function coming through the props
+        // from the ProjectDetailsPage, to refresh the project details
+        refreshProject();
+      })
+      .catch((error) => console.log(error));
+  };
+
+  
+  return (
+    <div className="AddTask">
+      <h3>Add New Task</h3>
+      
+      <form onSubmit={handleSubmit}>
+        <label>Title:</label>
+        <input
+          type="text"
+          name="title"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+
+        <label>Description:</label>
+        <textarea
+          type="text"
+          name="description"
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+        />
+
+        <button type="submit">Add Task</button>
+      </form>
+    </div>
+  );
+}
+
+export default AddTask;
---
+++ src/pages/ProjectDetailsPage.jsx
@@ -0,0 +1,62 @@
+// src/pages/ProjectDetailsPage.jsx
+
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { Link, useParams } from "react-router-dom";
+import AddTask from "../components/AddTask";
+import TaskCard from "../components/TaskCard";
+
+const API_URL = "https://project-management-api-4641927fee65.herokuapp.com"; 
+
+
+function ProjectDetailsPage () {
+  const [project, setProject] = useState(null);
+
+  const { projectId } = useParams()
+
+  const getProject = () => {   
+    console.log("Getting project")       //  <== ADD A NEW FUNCTION
+    axios
+      .get(`${API_URL}/projects/${projectId}?_embed=tasks`)
+      .then((response) => {
+        const oneProject = response.data;
+        console.log("Project ===>", oneProject)
+        setProject(oneProject);
+      })
+      .catch((error) => console.log(error));
+  };
+  
+  
+  useEffect(()=> {            // <== ADD AN EFFECT
+    getProject();
+  }, [] );
+
+  
+  return (
+    <div className="ProjectDetailsPage">
+      {project && (
+        <>
+          <h1>{project.title}</h1>
+          <p>{project.description}</p>
+        </>
+      )}
+
+      <AddTask refreshProject={getProject} projectId={projectId} />
+
+      {project &&
+        project.tasks.map((task) => (
+            <TaskCard key={task.id} {...task} />
+      ))}
+
+      <Link to="/projects">
+        <button>Back to projects</button>
+      </Link>
+
+      <Link to={`/projects/edit/${projectId}`}>
+        <button>Edit Project</button>
+      </Link>   
+    </div>
+  );
+}
+
+export default ProjectDetailsPage;
